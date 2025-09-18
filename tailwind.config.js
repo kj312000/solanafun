3c81ee08b348abc8361c06f373c90b273cc0f8e8
kj312000 <kaustubhjadhav36@gmail.com>
2025-09-18 20:11:03 +0530

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          dark: "#0f172a",
+          purple: "#9945FF",
+          teal: "#14F195",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
